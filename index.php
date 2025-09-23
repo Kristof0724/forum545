e0e1d677a3249c822a1b325eaeb8e1a371ac12e4
kk <kk>
2025-09-23 11:05:51 +0200

    delete fixed

--- index.php
+++ index.php
@@ -9,12 +9,16 @@
     }
 
 
- 
     
-    if(isset($_POST['topic'])){
+
+    
+    if(isset($_POST['action'])){
+        if($_POST['action']=='add')
+        {
        array_push($topics, $_POST['topic']);
        $jsonString = json_encode($topics);
        file_put_contents($fileName, $jsonString);
+        }
     }
 ?>
 <!DOCTYPE html>
@@ -30,11 +34,18 @@
     <ol>
     <?php
         foreach ($topics as $value) {
-            echo '<li>' . $value .'<input type=button value="törlés">';
+            echo '<li>' . $value .'
+            <form method="post">
+            <input type="hidden" name="topic"  value="'. $value. '">
+            <input type="hidden" name="action" value="delete">
+            <input type=submit value="törlés">
+            </form>
+            ';
         }
     ?>
     </ol>
     <form action="" method="post">
+        <input type="hidden" name="action" value="add">
         <input type="text" name="topic">
         <input type="submit" value="Add">
 
