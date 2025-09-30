e21c944a36abe695261e7fd69acada8eff878b29
kk <kk>
2025-09-30 11:10:02 +0200

    meg nem jo

--- index.php
+++ index.php
@@ -1,50 +1,52 @@
 <?php
-    $fileName = 'data.json';
-
-    if(file_exists($fileName)){
+     $fileName = 'data.json';
+    if (file_exists($fileName)) {
         $jsonString = file_get_contents($fileName);
         $topics = json_decode($jsonString);
-    }else{
+    } else {
         $topics = [];
     }
+    $time = date("H:i:s");
 
+    if (isset($_POST['action'])) {
 
-    
-
-    
-    if(isset($_POST['action'])){
-        $lastID = 0;
-        if(!empty($topics)){
-            $lastItem = end($topics);
-            $lastID = $lastItem->id;
-        }
-        $newID = $lastID+1;
-
-        if($_POST['action']=='add')
-        {
-            array_push($topics, 
+        # Új téma hozzáadása
+        if ($_POST['action'] == 'add') {
+            #region Az utolsó téma ID meghatározása
+            $lastId = 0;
+            if (!empty($topics)) {
+                $lastItem = end($topics);
+                $lastId = $lastItem->id;
+            } 
+            #endregion
+            array_push($topics,
             (object)[
-            "id" => $newID,
-            "name" => $_POST['topic']
-            ]);
-            $jsonString = json_encode($topics, JSON_PRETTY_PRINT);
-            file_put_contents($fileName, $jsonString);
-        } elseif (($_POST['action']=='delete')) {
-            $topicid = $_POST['id'];
-            foreach ($topics as $key => $value) {
-                if($value->id == $topicid){
-                    // unset($topics[$key]);
-                    // $topics = array_values($topics);
-                    array_splice($topics, $key,1);
-                    break;
-                }
-            }
-            $jsonString = json_encode($topics, JSON_PRETTY_PRINT);
-            file_put_contents($fileName, $jsonString);
+                "id" => $lastId + 1,
+                "name" => $_POST['topic'],
+                "time" => $time,
+            ]
+            );
+            $JsonString = json_encode($topics,JSON_PRETTY_PRINT);
+            file_put_contents($fileName,$JsonString);
         }
-
-
+        # Téma törlése
+        elseif (($_POST['action'] == 'delete')) 
+             if (isset($_POST['check'])) {
+            $id = $_POST['id'];
+            foreach ($topics as $key => $topic) {
+                if ($topic->id == $id) break;
+            }
+        
+            array_splice($topics,$key,1);
+            $JsonString = json_encode($topics,JSON_PRETTY_PRINT);
+            file_put_contents($fileName,$JsonString);
+             }else{
+            echo "nem lehet törölni!";
+        }
+        
+        
     }
+
 ?>
 <!DOCTYPE html>
 <html lang="hu">
@@ -54,26 +56,55 @@
     <title>Fórum</title>
 </head>
 <body>
-    
-    <h1>Témák:</h1>
-    <ol>
     <?php
-        foreach ($topics as $value) {
-            echo '<li>' . $value->name .'
+    if (!isset($_GET['topicID'])) {
+echo ' <h1>Témák:</h1>
+        <ol>';
+    
+            foreach ($topics as $value) {
+            echo '<li><a href="index.php?topicID=' . $value->id . '">'. $value->name . '</a>
             <form method="post">
-            <input type="hidden" name="id"  value="'. $value->id. '">
+            <input type="hidden" name="id" value="' . $value->id . '">
             <input type="hidden" name="action" value="delete">
-            <input type=submit value="törlés">
-            </form>
-            ';
+            <input type="checkbox" name="check">
+            <input type="submit" value="Törlés">
+            </form>';
+
         }
+        echo '</ol>';
+    } else {
+        $id = $_GET['topicID'];
+            foreach ($topics as $key => $topic) {
+                if ($topic->id == $id) break;
+            }
+        $topic = $topics[$key];
+        print_r($topics);
+        echo $topic-> name . '<br>';
+        echo '<a href=index.php>Vissza a témákhoz</a>';
+    }
     ?>
-    </ol>
     <form action="" method="post">
         <input type="hidden" name="action" value="add">
         <input type="text" name="topic">
         <input type="submit" value="Add">
 
+
+
+    </form>
+    <h1></h1>
+    <?php
+        if (isset($_GET['valami'])) {
+  echo "Az utolsó kapott érték: ".$_GET['comm'];        
+}
+
+    ?>
+    <form method="GET">
+        <input type="text" name="comm" placeholder="Név....">
+        <br>
+        <br>
+        <br>
+        <textarea name="" id=""placeholder="mondj véleményt" ></textarea>
+        <input type="submit" value="Küld">
 
 
     </form>
